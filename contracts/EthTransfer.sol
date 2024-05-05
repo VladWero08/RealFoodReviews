aafd266082a5de28933310ff4ce4307e9e0d99be
Iradu15 <Radu.Ionescu@pdq.com>
2024-05-05 18:22:58 +0300

    feat: add missing types of methods

--- contracts/EthTransfer.sol
+++ contracts/EthTransfer.sol
@@ -6,8 +6,20 @@ contract EthTransfer {
     
     function transferEther(address payable _to, uint256 _amount) external payable {
         require(address(this).balance >= _amount, "Insufficient balance in the contract");
-        _to.transfer(_amount);
+
+        uint256 newAmount = _pureMethod(_amount);
+        uint256 newAmount2 = _privateMethod(newAmount);
+
+        _to.transfer(newAmount2);
     }
 
     receive() external payable {}
+
+    function _pureMethod(uint256 _value) internal pure returns (uint256) {
+        return _value + 1;
+    }
+
+    function _privateMethod(uint256 _value) private pure returns (uint256) {
+        return _value - 1;
+    }
 }
