886b5ee8c02dacaa708889326cbfebb9925118c9
VladWero08 <vlad_wero@yahoo.com>
2024-05-18 17:37:42 +0300

    finished the review functionality

--- frontend/src/components/review/Review.jsx
+++ frontend/src/components/review/Review.jsx
@@ -6,7 +6,7 @@ export default function Reviews({index, rating, description}) {
             key={index} 
             className="review__wrapper"
         >
-            <h3>{rating} / 5 ⭐</h3>
+            <h3 style={{"padding": 0}}>{rating} / 5 ⭐</h3>
             <p>{description}</p>
         </div>
     )
--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,21 +1,21 @@
+
 import EthTransfer from './EthTransfer.json';
 import MyERC20 from './MyERC20.json';
 import Restaurant from './Restaurant.json';
 import Review from './Review.json';
 import User from './User.json';
 
-export const ethTransferAbi = EthTransfer.abi;
-export const ethTransferAddress = "undefined";
-
-export const myERC20Abi = MyERC20.abi;
-export const myERC20Address = "undefined";
-
 export const restaurantAbi = Restaurant.abi;
-export const restaurantAddress = "undefined";
-
-export const reviewAbi = Review.abi;
-export const reviewAddress = "undefined";
+export const restaurantAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 export const userAbi = User.abi;
-export const userAddress = "undefined";
-    
+export const userAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+export const myERC20Abi = MyERC20.abi;
+export const myERC20Address = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+
+export const reviewAbi = Review.abi;
+export const reviewAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+
+export const ethTransferAbi = EthTransfer.abi;
+export const ethTransferAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
