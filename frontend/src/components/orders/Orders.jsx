bace5b043e84dd7adb2594a9ccf3d56a053930be
VladWero08 <77508081+VladWero08@users.noreply.github.com>
2024-05-18 18:14:55 +0300

    small fixes related to the frontend and how orders are rendered (#13)

--- frontend/src/components/orders/Orders.jsx
+++ frontend/src/components/orders/Orders.jsx
@@ -12,6 +12,7 @@ export default function Orders() {
 
     const loadOrders = async() => {
         let orders = await myERC20Contract.methods.getOrdersByUser(address).call();        
+        console.log(orders);
         setMyOrders(orders);
     }
 
@@ -25,9 +26,12 @@ export default function Orders() {
 
             <div className="order-cards">
                 {myOrders.map((order, index) => (
-                    <div className="order-card">
-                        <h3>Paid <span className="purple">10FRF</span> to <span className="blue">0x2asfsdvawdvawrvafv</span></h3>
-                        <h5>Total products bought: 10</h5>
+                    <div 
+                        key={index}
+                        className="order-card"
+                    >
+                        <h3>Paid <span className="purple">{Number(order.amount)} FRF</span> to <span className="blue">{order.to}</span></h3>
+                        <h5>Total products bought: {order.products.length}</h5>
                     </div>
                 ))}
             </div>
